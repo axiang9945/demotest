49306b8a4cc179288d56647383e42f174b50929f
wucx <wucx@startdima.com.cn>
2017-12-26 10:36:14 +0800

    创建项目

---
+++ src/main/java/com/wcx/web/DemoMaven/App.java
@@ -0,0 +1,13 @@
+package com.wcx.web.DemoMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
